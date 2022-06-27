453cb38fce5298c8a51a689d63308c3d67391131
NorahMansour <norahalsheaifi@gmail.com>
2022-06-27 10:50:08 +0300

    donee

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import { server } from './server';
+import { server } from "./server";
 
-const port: any = process.env.$PORT ?? 3000
-
+const port: any = process.env.PORT ?? process.env.$PORT ?? 3000;
 server
-	.listen({
-		port: port,
-		host: '0.0.0.0',
-	})
-	.catch((err) => {
-		server.log.error(err);
-		process.exit(1);
-	});
+  .listen({
+    port: port,
+    host: "0.0.0.0",
+  })
+  .catch((err) => {
+    server.log.error(err);
+    process.exit(1);
+  });
